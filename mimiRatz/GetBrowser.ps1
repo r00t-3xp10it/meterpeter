1ea9cce668152f07eec43ac73a4c3382f1dbffd8
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-06-05 22:08:05 +0100

    Update GetBrowser.ps1
    added parameters to script execution

--- mimiRatz/GetBrowser.ps1
+++ mimiRatz/GetBrowser.ps1
@@ -11,8 +11,16 @@
    Language Used, Download Directory, URL History, Bookmarks, etc.. The dumps will be created in $env:tmp
 
 .EXAMPLE
-   PS C:\> ./GetBrowser.ps1
+   PS C:\> ./GetBrowser.ps1 -ALL
    Enumerates Internet Explorer, FireFox and Chrome Browsers info.
+
+.PARAMETERS
+   PS C:\> ./GetBrowser.ps1 -HELP
+   Displays GetBrowser.ps1 help discription.
+
+.PARAMETERS
+   PS C:\> ./GetBrowser.ps1 -FIREFOX
+   Enumerates FireFox Browser information Only.
 
 .LINK 
     https://github.com/r00t-3xp10it/meterpeter
@@ -20,22 +28,62 @@
     https://github.com/rvrsh3ll/Misc-Powershell-Scripts/blob/master/Get-BrowserData.ps1 (flagged by AV)
 #>
 
-
 $RFP = $null
 $Path = $null
 $JsPrefs = $null
 $RegPrefs = $null
+$param1 = $args[0]
 $ParsingData = $null
+
+
+## Help Menu (parameter)
+function HELP_MENU {
+  If($param1 -eq "-help"){
+    write-host "`n"
+    write-host ".Author r00t-3xp10it (SSA RedTeam @2020)" -ForegroundColor Green
+    write-host "  Required Dependencies: Local Web Browser (Installed)"
+    write-host "  Optional Dependencies: None"
+    write-host "`n"
+    write-host ".DESCRIPTION" -ForegroundColor Green
+    write-host "  Standalone Powershell script to dump Local-host browser information sutch as:"
+    write-host "  Home Page, Browser Version, ContryCode, Download Dir, URL History, Bookmarks,"
+    write-host "  etc.. The dumps will be Saved in `$env:tmp folder for later review."
+    write-host "`n"
+    write-host ".EXAMPLE" -ForegroundColor Green
+    write-host "  PS C:\> ./GetBrowser.ps1 -ALL"
+    write-host "  Enumerates Internet Explorer, FireFox and Chrome Browsers info."
+    write-host "`n"
+    write-host ".PARAMETERS" -ForegroundColor Green
+    write-host "  PS C:\> ./GetBrowser.ps1 -FIREFOX"
+    write-host "  Enumerates FireFox Browser information Only."
+    write-host "`n"
+    write-host ".LINK" -ForegroundColor Green
+    write-host "  https://github.com/r00t-3xp10it/meterpeter"
+    write-host "  https://github.com/r00t-3xp10it/meterpeter\mimRatz\GetBrowser.ps1"
+    exit
+  }
+}
+
+
 ## GetBrowser PS Script Banner
-Write-Host "GetBrowser - Dump Local-Host Browser(s) Information." -ForeGroundColor Green
+Write-Host "GetBrowser - Dump Local-Host Browsers Information." -ForeGroundColor Green
 Write-Host "[i] Dumping Data To: `$env:tmp\BrowserEnum.log" -ForeGroundColor yellow -BackgroundColor Black
 Start-sleep -Seconds 2
 
+## Get Default WebBrowser
+$Registry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $Env:ComputerName)
+$RegistryKey = $Registry.OpenSubKey("SOFTWARE\\Classes\\http\\shell\\open\\command")
+$Value = $RegistryKey.GetValue("") -replace '%1','' -replace '"',''
+echo "`n`nDefault Browser" > $env:tmp\BrowserEnum.log
+echo "---------------" >> $env:tmp\BrowserEnum.log
+echo "$Value" >> $env:tmp\BrowserEnum.log
+
 
 ## Retrieve IE Browser Information
+function IE_Dump {
 $IEVersion = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer" -Name 'Version' -ErrorAction SilentlyContinue|Select-Object 'Version'
 If(-not($IEVersion) -or $IEVersion -eq $null){
-    echo "`n`nIE Browser" > $env:tmp\BrowserEnum.log
+    echo "`n`nIE Browser" >> $env:tmp\BrowserEnum.log
     echo "----------" >> $env:tmp\BrowserEnum.log
     echo "Could not find any IE Browser Info .." >> $env:tmp\BrowserEnum.log
 }else{
@@ -46,11 +94,14 @@ If(-not($IEVersion) -or $IEVersion -eq $null){
     $ParsingData = $RegPrefs -replace '@{Start Page=','HomePage     : ' -replace '}',''
     $LocalPage = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Internet Explorer\Main\" -Name 'Search Page'|Select-Object 'Search Page'
     $ParsingLocal = $LocalPage -replace '@{Search Page=','SearchPage   : ' -replace '}',''
+    $IntSet = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\internet settings" -Name 'User Agent'|Select-Object 'User Agent'
+    $ParsingIntSet = $IntSet -replace '@{User Agent=','UserAgent    : ' -replace '}',''
     ## Build Remote LogFile
-    echo "`n`nIE Browser" > $env:tmp\BrowserEnum.log
+    echo "`n`nIE Browser" >> $env:tmp\BrowserEnum.log
     echo "----------" >> $env:tmp\BrowserEnum.log
     echo "$KBData" >> $env:tmp\BrowserEnum.log
     echo "$IEData" >> $env:tmp\BrowserEnum.log
+    echo "$ParsingIntSet" >> $env:tmp\BrowserEnum.log 
     echo "$ParsingData" >> $env:tmp\BrowserEnum.log
     echo "$ParsingLocal" >> $env:tmp\BrowserEnum.log
 }
@@ -90,9 +141,11 @@ ForEach ($URL in $URLs) {
         }
     }
 }
+}
 
 
 ## Retrieve FireFox Browser Information
+function FIREFOX {
 $Path = Test-Path "$env:APPDATA\Mozilla\Firefox\Profiles";
 If($Path -eq $True){
     ## change to the correct directory structure
@@ -150,9 +203,11 @@ If($Path -eq $False) {
         }
     }
 }
+}
 
 
 ## Get Google Chrome Version
+function CHROME {
 $Path = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Window\CurrentVersion\App Paths\chrome.exe' -ErrorAction SilentlyContinue
 If(-not($Path) -or $Path -eq $null){
     echo "`n`nChrome Browser" >> $env:tmp\BrowserEnum.log
@@ -165,9 +220,18 @@ If(-not($Path) -or $Path -eq $null){
     echo "--------------" >> $env:tmp\BrowserEnum.log
     echo "Version      : $GCVersion" >> $env:tmp\BrowserEnum.log
 }
+}
 
 
 ### ----
+
+## Jump Links (Functions)
+If(-not($param1)){HELP_MENU}
+If($param1 -eq "-IE"){IE_Dump}
+If($param1 -eq "-HELP"){HELP_MENU}
+If($param1 -eq "-FIREFOX"){FIREFOX}
+If($param1 -eq "-CHROME"){CHROME}
+If($param1 -eq "-ALL"){IE_Dump;FIREFOX;CHROME}
 
 
 ## Retrieve Remote Info from LogFile
