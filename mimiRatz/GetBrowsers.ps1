dbe22b65186c86aad09dadcfda04174c7cf32802
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-06-16 07:56:20 +0100

    Update GetBrowsers.ps1

--- mimiRatz/GetBrowsers.ps1
+++ mimiRatz/GetBrowsers.ps1
@@ -149,6 +149,9 @@ function IE_Dump {
     ## Retrieve IE Browser Information
     echo "`n`nIE Browser" >> $LogFilePath\BrowserEnum.log
     echo "----------" >> $LogFilePath\BrowserEnum.log
+    ## New MicrosoftEdge Update have changed the binary name to 'msedge' ..
+    $CheckVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer" -ErrorAction SilentlyContinue).version
+    If($CheckVersion -lt '9.11.18362.0'){$ProcessName = "MicrosoftEdge"}else{$ProcessName = "msedge"}
     $IEVersion = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer" -Name 'Version' -ErrorAction SilentlyContinue|Select-Object 'Version'
     If(-not($IEVersion) -or $IEVersion -eq $null){
         echo "Could not find any Browser Info .." >> $LogFilePath\BrowserEnum.log
@@ -168,8 +171,6 @@ function IE_Dump {
         $dataparse = "INetCache    : "+"$logfilefolder"
 
         ## New MicrosoftEdge Update have changed the binary name to 'msedge' ..
-        $CheckVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer" -ErrorAction SilentlyContinue).version
-        If($CheckVersion -lt '9.11.18362.0'){$ProcessName = "MicrosoftEdge"}else{$ProcessName = "msedge"}
         $IETestings = (Get-Process $ProcessName -ErrorAction SilentlyContinue).Responding
         If($IETestings -eq $True){$Status = "Status       : Active"}else{$Status = "Status       : Stoped"}
 
@@ -184,8 +185,6 @@ function IE_Dump {
     }
 
     ## Dump MicrosoftEdge.exe (OR: msedge.exe) binary path
-    $CheckVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer" -ErrorAction SilentlyContinue).version
-    If($CheckVersion -lt '9.11.18362.0'){$ProcessName = "MicrosoftEdge"}else{$ProcessName = "msedge"}
     $BinaryPath = Get-Process $ProcessName -ErrorAction SilentlyContinue
     If(-not($BinaryPath) -or $BinaryPath -eq $null){
         echo "BinaryPath   : requires $ProcessName process running to dump path." >> $LogFilePath\BrowserEnum.log
