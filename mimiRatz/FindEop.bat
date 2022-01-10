5ab8c15d7fcdcc8af30bb5242cdf4d3affcf4864
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-01-10 02:00:26 +0000

    meterpeter v2.10.11 - Spirit of Heaven, Goddess of Fire and Life!

--- mimiRatz/FindEop.bat
+++ mimiRatz/FindEop.bat
@@ -25,7 +25,7 @@ echo                         "(_/ / / / / / / / / / / / /|___/\ \ \ \ \ \ \ \ \ 
 echo                            "(_(_(_(_(_(_(_(_(_(_(_(_[_]_|_)_)_)_)_)_)_)_)_)_)_)_)"
 echo                                                     ^|    \
 echo                                                    / /   /___
-echo                                                   / /         "~~~~~__.
+echo                                                   / /         '~~~~~__.
 echo                                                   \_\_______________\_'_?
 echo                                       Spirit of Heaven, Goddess of Fire and Life
 echo. && echo. &&echo.
